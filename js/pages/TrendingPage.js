0d9bbeebad0bfce5195753a729365aea5efb5b54
username <1004928208@qq.com>
2019-03-06 20:54:18 +0800

    还原页面

--- js/pages/TrendingPage.js
+++ js/pages/TrendingPage.js
@@ -1,19 +1,22 @@
 import React, {Component} from 'react';
 import {StyleSheet, Text, View, Button} from 'react-native';
-import {connect} from 'react-redux';
-import actions from '../action/index';
 
 
-class TrendingPage extends Component {
+export default class FavoriterPage extends Component {
   render() {
     const {navigation} = this.props
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>TrendingPage</Text>
+        <Text style={styles.welcome}>FavoriterPage</Text>
         <Button
           title={'改变主题颜色'}
           onpress={()=>{
-            this.props.onThemeChange("#096")
+            navigation.setParams({
+              theme:{
+                tintColor: 'green',
+                updateTime: new Date().getTime()
+              }
+            })
           }}
         />
       </View>
@@ -34,12 +37,3 @@ const styles = StyleSheet.create({
     margin: 10,
   }
 });
-
-
-const mapStateToProps = (state) => ({})
-
-const mapDispatchToProps = (dispatch) => ({
-  onThemeChange: theme => dispatch(actions.onThemeChange(theme))
-})
-
-export default connect(mapStateToProps,mapDispatchToProps)(TrendingPage)
